cecad55dc2815150ebcb3d8afe47c697cf7dbcad
Mariano Ranucci <ranucci.mariano@gmail.com>
2023-05-11 12:26:12 -0300

    added seeds

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :product do
-    name { Faker::Lorem.word }
+    name { Faker::Commerce.product_name }
     description { Faker::Lorem.sentence }
-    price { Faker::Number.decimal(l_digits: 3) }
+    price { Faker::Commerce.price }
     stock { Faker::Number.between(from: 1, to: 10) }
   end
 end
