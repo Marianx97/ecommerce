2fe30ed9ecb420cd0f64a1466d55d1c79f8efbfa
Mariano Ranucci <ranucci.mariano@gmail.com>
2023-05-11 11:09:12 -0300

    added product validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
   has_many :orderables
   has_many :carts, through: :orderables
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :stock, numericality: { greater_than_or_equal_to: 0 }
 end
