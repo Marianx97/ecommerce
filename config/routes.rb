2000cc4faae450824f965d6207e1915117bc8e66
Mariano Ranucci <ranucci.mariano@gmail.com>
2023-05-11 09:01:17 -0300

    page navigation and routes changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  # Defines the root path route ("/")
+  root 'home#index'
+
+  # Defines devise user endpoints
   devise_for :users
+
+  # Resources
   resources :products, only: %i[index show]
-  resources :carts, only: %i[show]
   resources :purchases, only: %i[index show create]
+  resources :carts, only: %i[show]
   post 'carts/add_product'
   post 'carts/remove_product'
-
-  # Defines the root path route ("/")
-  root 'products#index'
 end
